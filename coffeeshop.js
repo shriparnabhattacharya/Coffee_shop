7681e011d10089c8206a5c835b06cce09dbae26f
SHRIPARNA BHATTACHARYA <78678455+shriparnabhattacharya@users.noreply.github.com>
2023-09-28 01:19:42 +0530

    Add files via upload

---
+++ coffeeshop.js
@@ -0,0 +1,18 @@
+let navbar = document.querySelector('.navbar-coffee');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-bar').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
